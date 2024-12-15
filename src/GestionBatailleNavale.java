1a13cc87e03cf17f8d1d8bedbe3f1b3f8032ff21
mq2vin <mcatan@iut.univ-paris8.fr>
2024-12-15 18:35:42 +0100

    Update Marvin (15/12/2024) GestionBatailleNavale.java

--- src/GestionBatailleNavale.java
+++ src/GestionBatailleNavale.java
@@ -1,20 +1,19 @@
-public class GestionBatailleNavale {
+public class GestionBatailleNaval {
+
+    public static char[][] plateaujoueur1 = new char[10][10];
+    public static char[][] plateaujoueur2 = new char[10][10];
+    public static char[][] plateaujoueur1cachee =new char[10][10];
+    public static char[][] plateaujouer2cachee =new char[10][10];
+
     public static void gestionBataille(){
-        char[][] plateauJoueur1 = new char[11][10];
-        char[][] plateauJoueur2 = new char[11][10];
-        char[][] plateauJoueur1Cachee = new char[11][10];
-        char[][] plateauJoueur2Cachee = new char[11][10];
-        Methodes.generationPlateau(plateauJoueur1);
-        Methodes.generationPlateau(plateauJoueur2);
-        Methodes.generationPlateau(plateauJoueur1Cachee);
-        Methodes.generationPlateau(plateauJoueur2Cachee);
 
-        Methodes.placementNavire(plateauJoueur1, 5, 9, 2 , 'h');
-        Methodes.afficherPlateau(plateauJoueur1);
+        Methodes.generationPlateau(plateaujoueur1);
+        Methodes.generationPlateau(plateaujoueur2);
+        Methodes.generationPlateau(plateaujoueur1cachee);
+        Methodes.generationPlateau(plateaujouer2cachee);
 
-        //Methodes.afficherPlateau(plateauJoueur1);
-        Methodes.textBatailleNavale();
-        Methodes.menuPrincipal();
+        Methodes.menu();
+
+
     }
-
 }
