3f3584f8bc94a2982b586887e31eb2091bbcae44
mq2vin <mcatan@iut.univ-paris8.fr>
2024-12-26 19:18:51 +0100

    Update Marvin 26/12/2024 MethodeTest.java

--- src/MethodeTest.java
+++ src/MethodeTest.java
@@ -7,75 +7,108 @@ public class MethodeTest {
     public void testTousCoules() {
 
         char[][] plateau1 = {
-                {'~', '~', '~'},
-                {'~', '~', '~'},
-                {'~', '~', '~'}
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'}
         };
-        assertTrue(Methodes.tousCoulés(plateau1), "Tous les navires sont coulés, devrait retourner true.");
+        assertTrue(Methodes.tousCoules(plateau1), "Il n'y a pas de navire sur un plateau \"vide\", devrait retourner true.");
 
         char[][] plateau2 = {
-                {'P', 'C', 'T'},
-                {'C', 'T', 'C'},
-                {'T', 'P', 'C'}
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', 'P','P', 'P', 'P','P', '~', '~', 'T'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', 'T'},
+                {'~', 'c', 'c','c', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', 'C', 'C','C', 'C', '~', '~'},
+                {'~', '~', 'S','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', 'S','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', 'S','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'}
         };
-        assertFalse(Methodes.tousCoulés(plateau2), "Aucun navire n'est coulé, devrait retourner false.");
+        assertFalse(Methodes.tousCoules(plateau2), "Aucun navire n'est coulé, devrait retourner false.");
 
         char[][] plateau3 = {
-                {'~', '~', '~'},
-                {'~', 'C', '~'},
-                {'~', '~', '~'}
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','X', 'X', 'X','X', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', 'X', 'X','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', 'C', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', 'C', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', 'C', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','X', 'X', 'X', '~'}
         };
-        assertFalse(Methodes.tousCoulés(plateau3), "Un navire n'est pas coulé, devrait retourner false.");
+        assertFalse(Methodes.tousCoules(plateau3), "Un navire (non touché) n'est pas coulé, devrait retourner false.");
 
         char[][] plateau4 = {
-                {'P', '~', '~'},
-                {'~', 'C', '~'},
-                {'~', '~', 'T'}
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','X', 'X', 'X','X', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', 'X', 'X','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', 'C', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', 'X', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', 'X', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','X', 'X', 'X', '~'}
         };
-        assertFalse(Methodes.tousCoulés(plateau4), "Des navires sont encore sur le plateau, devrait retourner false.");
+        assertFalse(Methodes.tousCoules(plateau4), "un navire (touché) n'est pas coulé, devrait retourner false.");
+
+        char[][] plateau5 = {
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','X', 'X', 'X','X', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', 'X', 'X','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', 'X', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', 'X', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', 'X', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', 'X', '~', '~'},
+                {'~', '~', '~','~', '~', '~','~', '~', '~', '~'},
+                {'~', '~', '~','~', '~', '~','X', 'X', 'X', '~'}
+        };
+        assertTrue(Methodes.tousCoules(plateau5), "Tous Coulés, devrait retourner true.");
     }
 
     @Test
     public void testPlacementNavire() {
         char[][] plateau1 = new char[10][10];
-        Methodes.generationPlateau(plateau1);
-
-        assertTrue(Methodes.placementNavire(plateau1, 3, 1, 2, 'h'), "Placement horizontal valide, devrait retourner true.");
-        assertEquals('c', plateau1[2][1], "Le navire doit être correctement placé à la position [2][1].");
-        assertEquals('c', plateau1[2][2], "Le navire doit être correctement placé à la position [2][2].");
-        assertEquals('c', plateau1[2][3], "Le navire doit être correctement placé à la position [2][3].");
-
-
         char[][] plateau2 = new char[10][10];
-        Methodes.generationPlateau(plateau2);
-
-        assertTrue(Methodes.placementNavire(plateau2, 4, 2, 0, 'v'), "Placement vertical valide, devrait retourner true.");
-        assertEquals('C', plateau2[0][2], "Le navire doit être correctement placé à la position [0][2].");
-        assertEquals('C', plateau2[1][2], "Le navire doit être correctement placé à la position [1][2].");
-        assertEquals('C', plateau2[2][2], "Le navire doit être correctement placé à la position [2][2].");
-        assertEquals('C', plateau2[3][2], "Le navire doit être correctement placé à la position [3][2].");
-
-
-
         char[][] plateau3 = new char[10][10];
-        Methodes.generationPlateau(plateau3);
-        Methodes.placementNavire(plateau3, 4, 1, 2, 'h');
-
-        assertFalse(Methodes.placementNavire(plateau3, 3, 1, 2, 'h'), "Collision détectée, devrait retourner false.");
-
-
-
         char[][] plateau4 = new char[10][10];
-        Methodes.generationPlateau(plateau4);
 
-        assertFalse(Methodes.placementNavire(plateau4, 5, 10, 2, 'h'), "Placement hors limites horizontal, devrait retourner false.");
+        Methodes.generationDesPlateau(plateau1,plateau2,plateau3,plateau4);
+
+        //placement horizontal d'un contre-torpilleur
+        assertTrue(Methodes.placementNavire(plateau1, 3,2, 1, 2, 'h'), "Placement horizontal valide, devrait retourner true.");
+        assertEquals('c', plateau1[2][1]);
+        assertEquals('c', plateau1[2][2]);
+        assertEquals('c', plateau1[2][3]);
 
 
+        //placement vertical d'un croiseur
+        assertTrue(Methodes.placementNavire(plateau2, 4, 1,2, 0, 'v'), "Placement vertical valide, devrait retourner true.");
+        assertEquals('C', plateau2[0][2]);
+        assertEquals('C', plateau2[1][2]);
+        assertEquals('C', plateau2[2][2]);
+        assertEquals('C', plateau2[3][2]);
 
-        char[][] plateau5 = new char[10][10];
-        Methodes.generationPlateau(plateau5);
 
-        assertFalse(Methodes.placementNavire(plateau5, 5, 2, 10, 'v'), "Placement hors limites vertical, devrait retourner false.");
+        //collision
+        Methodes.placementNavire(plateau3, 4, 1,1, 2, 'h');
+        assertFalse(Methodes.placementNavire(plateau3, 3, 3,1, 2, 'h'), "Collision détectée, devrait retourner false.");
+
+        //placement hors limites
+        assertFalse(Methodes.placementNavire(plateau4, 5,0, 10, 2, 'h'), "Placement hors limites horizontal, devrait retourner false.");
+
     }
+
 
 }
